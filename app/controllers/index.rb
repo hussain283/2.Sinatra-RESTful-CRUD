489de18b4303da4f2fb62ce708654986f1efa33a
Hussain Tambawala <hussain283@gmail.com>
2013-06-27 02:01:59 -0700

    REST

---
+++ app/controllers/index.rb
@@ -0,0 +1,41 @@
+get '/' do
+	@notes = Note.order('title ASC')
+	erb :index
+end
+
+get '/note/new' do
+	@note = Note.new
+	erb :note_form
+end
+
+post '/note/create' do
+	@note = Note.new(params[:note])
+	if @note.valid?
+		@note.save
+		redirect '/'
+	else
+		erb :note_form
+	end
+end
+
+get '/note/edit/:id' do
+	@note = Note.find(params[:id])
+	p @note
+	erb :note_form
+end
+
+post '/note/update/:id' do 
+	@note = Note.find(params[:id])
+	@note.assign_attributes(params[:note])
+	if @note.valid?
+		@note.save
+		redirect '/'
+	else
+		erb :note_form
+	end
+end
+
+get '/note/delete/:id' do
+	Note.delete(params[:id])
+	redirect '/'
+end
---
+++ db/migrate/20130627011137_create_notes.rb
@@ -0,0 +1,8 @@
+class CreateNotes < ActiveRecord::Migration
+  def change
+  	create_table :notes do |t|
+  		t.string :title, :content
+  		t.timestamps
+  	end
+  end
+end
